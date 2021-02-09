ca5f4d7f71b08cd35872d6dd11cfe06fb868ad42
zmarkan <zan@circleci.com>
2021-02-09 13:33:36 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -2,47 +2,63 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 
 import './App.css';
-import { getState, setState } from 'react'
+import { useState, createRef} from 'react'
 import { Form, Container, Button, NavLink} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {   } from "@fortawesome/fontawesome-svg-core";
 import { faLanguage } from "@fortawesome/free-solid-svg-icons";
-import { } from "@fortawesome/fontawesome-free";
-import background from './background.jpg'
-// import 
+
+
 
 function App() {
+
+  const [translationInput, setTranslationInput] = useState('')
+  const [translationOutput, setTranslationOutput] = useState('')
+  const inputRef = createRef()
+
+  const submitTranslation = async e => {
+    e.preventDefault()
+    let stringToTranslate = inputRef.current.value
+
+    let transl8Response = await fetch({
+      url: 'http://api.translate.demo.gs/translate', //Inject value
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      body: stringToTranslate
+    })
+
+    console.log(transl8Response)
+    
+    
+  }
+
   return (
     <div className="App">
 
 <Container className="Content-container">
   <header className="App-Header">
       <h1>Cloudn8-Transl8</h1>
+
+      <h4>🇦🇺🇧🇷🇨🇿🇪🇹🇫🇰🇬🇮🇭🇳🇮🇶🇯🇴🇰🇪🇰🇵🇨🇺🇮🇪🇺🇳🇿🇲🇱🇾🇦🇪🇲🇳🇳🇵🇨🇭🇲🇩🇦🇲</h4>
       <h4>The only translation service your business will ever need</h4>
       <h5>Powered by TF, CI/CD, K8S, AI, ML, and most importantly C8H10N4O2</h5>
-      
+      <br/>
   </header>
-<Form>
-  <Form.Group controlId="translateForm.translationInput">
-    {/* <Form.Label>Enter the sentence you wish to translate</Form.Label> */}
-    <Form.Control as="textarea" rows={3} placeholder="Enter the sentence you wish to translate. For example, typing in I am milk" />
-  </Form.Group>
 
-  <Button variant="primary" type="submit">
+  <Form.Control ref={inputRef} name="input" as="textarea" rows={3} placeholder="Enter the sentence you wish to translate. For example, typing in I am milk" />
+  <br/>
+  <Button variant="primary" type="submit" onClick={submitTranslation}>
     <FontAwesomeIcon icon={faLanguage} />
     {/* <FontAwesomeIcon icon={translate} fixedWidth border /> */}
   </Button>
 
-  <Form.Group controlId="translateForm.translationOutput">
-    <Form.Label>Translation output</Form.Label>
-    <Form.Control as="textarea" rows={3} placeholder="¡Soy milk!"/>
-  </Form.Group>
+    <br/>
+    <br/>
 
-  
+  <Form.Control name="output" as="textarea" rows={3} placeholder="¡Soy milk!" value={translationOutput}/>
 
-
-</Form>
-
+    <br/>
 <footer>
   Source on <a href="https://github.com/cloudn8-transl8">github.com/cloudn8-transl8</a> <br/>Proudly overengineered in London by <a href="https://twitter.com/sheriffjackson">@sheriffjackson</a> and <a href="https://twitter.com/zmarkan">@zmarkan</a>
 </footer>
