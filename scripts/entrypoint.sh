e4e6dbe0634a9e1b28855ea12fdaa5c520e31f4c
zmarkan <zan@circleci.com>
2021-02-11 13:14:56 +0000

    test re-test entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,6 +8,8 @@ then
     echo "ERROR injecting environment variables to React app: \$API_ENDPOINT environment variable must be set."
     exit 1
 fi
+echo "API ENDPOINT IN ENTRYPOINT"
+echo $API_ENDPOINT
 
 # Add assignment 
 cat <<EOF > /usr/share/nginx/html/env-config.js
